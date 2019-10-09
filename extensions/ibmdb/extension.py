c38b69c33c30e935337c2004f29f3ab5938112ea
Damian Kolesnik <damian.kolesnik@pl.ibm.com>
2019-10-10 00:05:09 +0200

    Remove function which downloads ibm extensions

--- extensions/ibmdb/extension.py
+++ extensions/ibmdb/extension.py
@@ -156,7 +156,8 @@ class IBMDBInstaller(ExtensionHelper):
         self._compilationEnv['IBM_DB_HOME'] = self._ctx['IBMDBCLIDRIVER_INSTALL_DIR']
         self._logMsg('-- Installed IBM DB CLI Drivers ------------------')
         self._logMsg('--Install devel---')
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['apt-get install php7.2-dev'])
+        #self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['apt-get install php7.2-dev'])
+        cmd = exec.Command("sudo apt-get install php7.2-dev")
 
     def cleanup(self):
         self._logMsg('-- Some House-keeping ----------------------------')
