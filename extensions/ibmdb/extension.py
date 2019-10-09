cb423f1251a7651165c9ffd4ba981b8688c91b02
Damian Kolesnik <damian.kolesnik@pl.ibm.com>
2019-10-09 23:05:06 +0200

    Remove download extension which not works

--- extensions/ibmdb/extension.py
+++ extensions/ibmdb/extension.py
@@ -13,17 +13,6 @@ PKGDOWNLOADS =  {
     #CLI_DRIVER
      'IBMDBCLIDRIVER1_DLFILE': 'linuxx64_odbc_cli.tar.gz',
      'IBMDBCLIDRIVER1_DLURL': 'https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/{IBMDBCLIDRIVER1_DLFILE}',
-    
-    #IBM_DB Packages  
-    'IBM_DB2_VERSION': '1.9.9',
-    'IBM_DB2_REPOSITORY': 'https://github.com/ibmdb/php-buildpack',
-    'IBM_DB2_DLFILE': 'ibm_db2-v{IBM_DB2_VERSION}.tar.gz',
-    'IBM_DB2_DLURL': '{IBM_DB2_REPOSITORY}/raw/master/extensions/ibmdb/Binaries/{IBM_DB2_DLFILE}',
-
-    'PDO_IBM_VERSION': '1.3.4',
-    'PDO_IBM_REPOSITORY': 'https://github.com/ibmdb/php-buildpack',
-    'PDO_IBM_DLFILE': 'pdo_ibm-v{PDO_IBM_VERSION}.tar.gz',
-    'PDO_IBM_DLURL': '{PDO_IBM_REPOSITORY}/raw/master/extensions/ibmdb/Binaries/{PDO_IBM_DLFILE}',
 }
 
 class IBMDBInstaller(ExtensionHelper):
